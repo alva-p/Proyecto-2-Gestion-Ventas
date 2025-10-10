d32003fe1d9fa3ca3de01e26812023a9ed7b3900
AlvaroP1017 <alvaropineda1017@gmail.com>
2025-10-10 00:15:13 -0300

    cambios puertos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -33,7 +33,7 @@ import { Marca } from './marcas/entities/marca.entity';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '6543', 10),
+      port: parseInt(process.env.DB_PORT || '5432', 10),
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
